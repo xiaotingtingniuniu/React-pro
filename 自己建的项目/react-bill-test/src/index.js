1580a01126d10e47eed9635029575104ef41feb2
xiaotingtingniuniu <424338316@qq.com>
2025-03-01 22:37:11 +0800

    完成主题颜色的配置

--- 自己建的项目/react-bill-test/src/index.js
+++ 自己建的项目/react-bill-test/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import sum from '@/test';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+//导入定制主题颜色文件
+import './theme.css'
 const total = sum(1,3);
 console.log('total',total);
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- 自己建的项目/react-bill-test/src/pages/Layout/index.js
+++ 自己建的项目/react-bill-test/src/pages/Layout/index.js
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom"
-
+import { Button } from "antd-mobile"
 const Layout = ()=>{
     return(
         <div>
             {/* 二级路由出口 */}
             <Outlet />
             我是Layout
+            <Button color="primary">测试全局按钮</Button>
+            <div className="purple-theme">
+                <Button color="primary">测试局部按钮</Button>
+            </div>
         </div>
     )
 }
