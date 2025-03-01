b2e6c7882b3638721cab1b8b6a557c855531af55
xiaotingtingniuniu <424338316@qq.com>
2025-03-01 20:44:14 +0800

    完成别名配置

--- 自己建的项目/react-bill-test/src/index.js
+++ 自己建的项目/react-bill-test/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-
+import sum from '@/test';
+const total = sum(1,3);
+console.log('total',total);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
