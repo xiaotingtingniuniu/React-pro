379df7882f746114ce6067477f6233cbe2d85167
xiaotingtingniuniu <424338316@qq.com>
2025-03-02 16:29:21 +0800

    月度账单--切换时间显示

--- 自己建的项目/react-bill-test/src/pages/Month/index.js
+++ 自己建的项目/react-bill-test/src/pages/Month/index.js
@@ -5,8 +5,17 @@ import classNames from 'classnames'
 const Month = () => {
   //控制弹框的打开和关闭
   const [dateVisible,setDateVisible] = useState(false);
-  const onConfirm = () => {
-    console.log(111);
+  //控制时间显示
+  const [currentDate,setCurrentDate] = useState(()=>{
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth();
+    return year+' | '+month;
+  });
+  const onConfirm = (val) => {
+    const year = val.getFullYear();
+    const month = val.getMonth()+1;
+    const totalDate = year+' | '+month;
+    setCurrentDate(totalDate);
   }
   return (
     <div className="monthlyBill">
@@ -18,7 +27,7 @@ const Month = () => {
           {/* 时间切换区域 */}
           <div className="date" onClick={()=>setDateVisible(true)}>
             <span className="text">
-              2023 | 3月账单
+              {currentDate}月账单
             </span>
             {/* 有expand类名 箭头潮上 */}
             <span className={classNames('arrow',dateVisible && 'expand')}></span>
@@ -48,7 +57,7 @@ const Month = () => {
             onCancel={()=>setDateVisible(false)}
             onClose={()=>setDateVisible(false)}
             closeOnMaskClick={()=>setDateVisible(false)}
-            onConfirm={onConfirm}
+            onConfirm={(val)=>onConfirm(val)}
           />
         </div>
       </div>
