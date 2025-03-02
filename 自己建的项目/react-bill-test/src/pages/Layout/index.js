1c918b259a215c2d6aaba14aead94c4f36ec5b15
xiaotingtingniuniu <424338316@qq.com>
2025-03-02 11:08:52 +0800

    实现TabBar切换路由

--- 自己建的项目/react-bill-test/src/pages/Layout/index.js
+++ 自己建的项目/react-bill-test/src/pages/Layout/index.js
@@ -1,8 +1,27 @@
-import { Outlet } from "react-router-dom"
-import { Button } from "antd-mobile"
+import { TabBar } from "antd-mobile"
+import { Outlet,useNavigate } from "react-router-dom"
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { fetchBillList } from "@/store/modules/billStore"
+import {BillOutline,CalculatorOutline,AddCircleOutline} from 'antd-mobile-icons'
+import './index.scss'
+const tabs = [
+    {
+      key: '/',
+      title: '月度账单',
+      icon: <BillOutline />
+    },
+    {
+        key: '/new',
+        title: '记账',
+        icon: <AddCircleOutline />
+    },
+    {
+        key: '/year',
+        title: '年度账单',
+        icon: <CalculatorOutline />
+    }
+]
 const Layout = ()=>{
     //获取dispatch函数
     const dispatch = useDispatch();
@@ -10,16 +29,27 @@ const Layout = ()=>{
     useEffect(()=>{
         //修改store中的数据
         dispatch(fetchBillList());
-    },[dispatch])
+    },[dispatch]);
+    //获取navigate函数
+    const navigate = useNavigate();
+    // 切换路由
+    const switchRoute = (path) => {
+       //路由跳转
+       navigate(path);
+    }
     return(
-        <div>
-            {/* 二级路由出口 */}
-            <Outlet />
-            我是Layout
-            <Button color="primary">测试全局按钮</Button>
-            <div className="purple-theme">
-                <Button color="primary">测试局部按钮</Button>
+        <div className="layout">
+            <div className="container">
+                {/* 二级路由出口 */}
+                <Outlet />
             </div>
+            <div className="footer">
+                <TabBar onChange={switchRoute}>
+                    {tabs.map(item => (
+                      <TabBar.Item key={item.key} icon={item.icon} title={item.title}/>
+                    ))}
+                </TabBar>
+            </div> 
         </div>
     )
 }
