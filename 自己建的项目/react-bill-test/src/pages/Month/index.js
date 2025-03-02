f73d8e9a398af7dc91e1cb51f3f1f2f04dbe914c
xiaotingtingniuniu <424338316@qq.com>
2025-03-02 14:35:12 +0800

    修改月度账单的弹框切换功能

--- 自己建的项目/react-bill-test/src/pages/Month/index.js
+++ 自己建的项目/react-bill-test/src/pages/Month/index.js
@@ -1,8 +1,59 @@
-const Month = ()=>{
-    return(
-        <div>
-            我是Month
+import { NavBar, DatePicker} from 'antd-mobile'
+import './index.scss'
+import { useState } from 'react'
+import classNames from 'classnames'
+const Month = () => {
+  //控制弹框的打开和关闭
+  const [dateVisible,setDateVisible] = useState(false);
+  const onConfirm = () => {
+    console.log(111);
+  }
+  return (
+    <div className="monthlyBill">
+      <NavBar className="nav" backIcon={false}>
+        月度收支
+      </NavBar>
+      <div className="content">
+        <div className="header">
+          {/* 时间切换区域 */}
+          <div className="date" onClick={()=>setDateVisible(true)}>
+            <span className="text">
+              2023 | 3月账单
+            </span>
+            {/* 有expand类名 箭头潮上 */}
+            <span className={classNames('arrow',dateVisible && 'expand')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className="item">
+              <span className="money">{100}</span>
+              <span className="type">支出</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">收入</span>
+            </div>
+            <div className="item">
+              <span className="money">{200}</span>
+              <span className="type">结余</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className="kaDate"
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            max={new Date()}
+            onCancel={()=>setDateVisible(false)}
+            onClose={()=>setDateVisible(false)}
+            closeOnMaskClick={()=>setDateVisible(false)}
+            onConfirm={onConfirm}
+          />
         </div>
-    )
+      </div>
+    </div >
+  )
 }
+
 export default Month
